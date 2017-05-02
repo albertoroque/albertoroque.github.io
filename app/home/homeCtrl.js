782dde65290d125e9ecf4997d0dbdba916091186
Alberto Roque <alberto.roque097@hotmail.com>
2017-05-01 22:37:55 -0300

    Function Decode Location

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -37,7 +37,8 @@ angular.module('proj.home', [])
         
 
     }
-
+    
+    
     $scope.loadSlider = function (firstImage) {
 
         
@@ -93,7 +94,68 @@ angular.module('proj.home', [])
     }
 
     
+    var geocoder = new google.maps.Geocoder;
 
+    //Extrai determinada parte do endereço completo.
+    function extractFromAddress(components, type) {
+        for (var i = 0; i < components.length; i++)
+            for (var j = 0; j < components[i].types.length; j++)
+                if (components[i].types[j] == type) return components[i].long_name;
+        return "";
+    }
+    
+    $scope.getLocation(){
+        if (navigator.geolocation) {
+           navigator.geolocation.getCurrentPosition(showPosition, showError);
+        }
+        else { $scope.locationDecoded = "Seu browser não suporta Geolocalização."; }
+    }
+
+    //Mostra as coordenadas - lat/lon e mostra o bairro + cidade.
+    function showPosition(position) {
+        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) 
+            {
+                if (results[0]) 
+                {
+
+                	console.log(results[0]);
+                    var cidade = extractFromAddress(results[0].address_components, "locality");
+                    var bairro = extractFromAddress(results[0].address_components, "sublocality_level_1");
+                    var est = extractFromAddress(results[0].address_components, "administrative_area_level_1");
+                    var pais = extractFromAddress(results[0].address_components, "country");
+
+                    console.log(cidade, bairro);                    
+
+                    //x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+                    $scope.locationDecoded = "Bairro: " + bairro + "<br> Cidade: " + cidade + "<br> Estado e Pais: "+ est + " - " + pais	;
+                }
+            } 
+            else 
+            {
+            	alert("Geocode was not successful for the following reason: " + status)
+            }
+        });
+    }
+
+    //Mostra erro, caso não seja possível utilizar a API, por N motivos.
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("Usuário rejeitou a solicitação de Geolocalização.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Localização indisponível.");
+                break;
+            case error.TIMEOUT:
+                alert("O tempo da requisição expirou.");
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("Algum erro desconhecido aconteceu.");
+                break;
+        }
+    }
 
    
 });
